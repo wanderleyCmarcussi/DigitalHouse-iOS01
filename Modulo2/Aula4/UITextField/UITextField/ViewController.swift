fe6e96b739da727debfbd6256762cbe834d0f85d
Wanderley Marcussi <wmarcussi@me.com>
2019-05-11 11:54:46 -0300

    primeira aula com UITextFields com Delegate

--- Modulo2/Aula4/UITextField/UITextField/ViewController.swift
+++ Modulo2/Aula4/UITextField/UITextField/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField.isEqual(self.emailTextField) {
+            self.passwordTextField.becomeFirstResponder()
+        } else {
+                self.passwordTextField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.backgroundColor = .green
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.backgroundColor = .gray
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing")
+        return true
+    }
+    
 }
 
