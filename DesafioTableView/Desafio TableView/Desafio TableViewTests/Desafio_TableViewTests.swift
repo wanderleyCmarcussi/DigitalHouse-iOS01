c01a0df8bbdcdb7f0bde9a43de5b3e492d6f8a91
Wanderley Marcussi <wmarcussi@me.com>
2019-05-15 23:05:03 -0300

    Novo projeto TableView

---
+++ DesafioTableView/Desafio TableView/Desafio TableViewTests/Desafio_TableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Desafio_TableViewTests.swift
+//  Desafio TableViewTests
+//
+//  Created by Wanderley Marcussi on 15/05/19.
+//  Copyright © 2019 Wanderley Marcussi. All rights reserved.
+//
+
+import XCTest
+@testable import Desafio_TableView
+
+class Desafio_TableViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
